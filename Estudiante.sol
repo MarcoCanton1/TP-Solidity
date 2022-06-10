bc5b73675ece3fbcd7ed12548f9130062180c186
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-06-10 08:14:09 -0300

    Actualización código

--- Estudiante.sol
+++ Estudiante.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.10;
  
 contract Estudiante{
-   
+    
     string private _nombre;
     string private _apellido;
     string private _curso;
@@ -23,10 +23,32 @@ contract Estudiante{
     }
  
     function nombre_completo() public view returns (string memory){
-        return string(bytes.concat(bytes(_nombre), " ", bytes(_apellido)));
+        return string(bytes.concat(bytes(_nombre), " ", bytes(_apellido))); // convierte los 2 strings en bytes, los une y los vuelve a convertir en string
     }
  
     function curso() public view returns (string memory){
-        return _curso;
+        return _curso; // devuelve el curso
+    }
+ 
+    // el docente establece la nota de cada materia
+    function set_nota_materia(uint8 nota, string memory materia) public{
+        require(msg.sender == _docente, "Solo el docente puede poner notas"); // revisa que el que mande la nota sea el docente registrado y sino devuelve un mensaje de error
+        notas_materias[materia] = nota; // guarda en el array el valor (nota) de la key insertada (materia)
+    }
+ 
+ 
+    function nota_materia(string memory materia){
+        return notas_materias[materia]; // pide del array notas_materias el valor de la key materia (la nota de la materia pedida)
+    }
+ 
+    function aprobo(string memory materia){
+        if(notas_materias[materia] >= 60)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
